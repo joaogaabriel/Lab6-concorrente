0e98c53e6708fd2aac081ed3dfbce01cc6d663ac
marcelodelazari <marceloalvesdelazari@gmail.com>
2024-09-26 16:48:17 -0300

    comecando taskproducer

--- lab6/java/scenario1/TaskProducer.java
+++ lab6/java/scenario1/TaskProducer.java
@@ -1,5 +1,24 @@
-package java.scenario1;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class TaskProducer {
+
+public class TaskProducer implements Runnable{
+
+    private final BlockingQueue<Task> taskQueue;
+    private final int productionTime;
+    private final AtomicInteger producerId;
+    private static AtomicInteger idCounter = new AtomicInteger(0);
+
+    public TaskProducer(BlockingQueue<Task> taskQueue, int productionTime){
+        this.taskQueue = taskQueue;
+        this.productionTime = productionTime;
+        this.producerId = new AtomicInteger(idCounter.incrementAndGet());
+    }
+    
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
     
 }
