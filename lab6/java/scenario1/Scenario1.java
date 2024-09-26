195b1936cb7f0fb33b2c3e68e3f0d206b4617cc0
marcelodelazari <marceloalvesdelazari@gmail.com>
2024-09-26 17:04:29 -0300

    terminando scenario1.java

--- lab6/java/scenario1/Scenario1.java
+++ lab6/java/scenario1/Scenario1.java
@@ -13,5 +13,21 @@ public class Scenario1 {
         for(int i = 0; i < 5; ++i){
             new Thread(new TaskProducer(taskQueue, 5000)).start();
         }
+
+        for(int i = 0; i < 3; i++){
+            new Thread(new Node(taskQueue, i + 1)).start();
+        }
+
+        new Thread(() -> {
+            while(true){
+                try{
+                    Thread.sleep(5000); //5 segundos
+                    //logica
+                }
+                catch(InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
     }
 }
