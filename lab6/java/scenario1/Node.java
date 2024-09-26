7066ea6c5e2693fcc218b9dcb3bc7c9b1c8dfba7
Mateussnts <mateus.santos@ccc.ufcg.edu.br>
2024-09-26 17:01:36 -0300

    Implementand clase Node

--- lab6/java/scenario1/Node.java
+++ lab6/java/scenario1/Node.java
@@ -1,5 +1,29 @@
 package java.scenario1;
 
-public class Node {
+import java.util.concurrent.BlockingQueue;
+
+public class Node implements Runnable{
     
+    private final BlockingQueue<Task> taskQueue;
+    private final int nodeId;
+
+    public Node(BlockingQueue<Task> taskQueue, int nodeId){
+        this.taskQueue = taskQueue;
+        this.nodeId = nodeId;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while (true) {
+                Task task = taskQueue.take();
+                long waitingTime = System.currentTimeMillis() - task.getCreationTime();
+                System.out.println("Node " + nodeId + "Processando tarefa " + task.getTaskId() + "do produtor" + task.getProducerId() + ". Tempo em fila: " + waitingTime + "ms.");
+                task.execute();
+            }
+        } catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
+
 }
