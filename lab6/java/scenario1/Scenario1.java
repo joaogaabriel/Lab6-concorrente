16ba4e66b07611c17163d6978a5abe3f11336ee5
marcelodelazari <marceloalvesdelazari@gmail.com>
2024-09-26 16:58:42 -0300

    criando produtores

--- lab6/java/scenario1/Scenario1.java
+++ lab6/java/scenario1/Scenario1.java
@@ -1,5 +1,17 @@
 package java.scenario1;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Scenario1 {
     
+    public static void main(String[] args){
+
+        BlockingQueue<Task> taskQueue = new LinkedBlockingQueue<>();
+
+        //criando 5 produtores com intervalo 5000ms
+        for(int i = 0; i < 5; ++i){
+            new Thread(new TaskProducer(taskQueue, 5000)).start();
+        }
+    }
 }
