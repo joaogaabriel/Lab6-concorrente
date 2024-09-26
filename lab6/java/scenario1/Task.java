3ebd91bc9a0ba13c0df1086031f9cfbdd3af5b22
joaogaabriel <joaogabriel13ferreira@gmail.com>
2024-09-26 16:56:41 -0300

    implementação classe Task

--- lab6/java/scenario1/Task.java
+++ lab6/java/scenario1/Task.java
@@ -1,5 +1,32 @@
 package java.scenario1;
 
 public class Task {
+    private final int producerId;
+    private final long taskId;
+    private final long creationTime;
+
+    public Task(int producerId){
+        this.producerId = producerId;
+        this.taskId = System.currentTimeMillis();
+        this.creationTime = System.currentTimeMillis();
+    }
+
+    public void execute(){
+        long executionTime = (long) (Math.random() * (15000 - 1000)) + 1000;
+        try {
+            Thread.sleep(executionTime);
+        } catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
+    public long getTaskId(){
+        return taskId;
+    }
+    public long getCreationTime(){
+        return creationTime;
+    }
+    public int getProducerId(){
+        return producerId;
+    }
     
 }
