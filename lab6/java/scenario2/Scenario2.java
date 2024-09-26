1e2725d480c878f9ceeae93986b0d3f195317402
Mateussnts <mateus.santos@ccc.ufcg.edu.br>
2024-09-26 17:28:34 -0300

    adicionando esqueleto do segundo cenario

---
+++ lab6/java/scenario2/Scenario2.java
@@ -0,0 +1,7 @@
+public class Scenario2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Scenario Base of Lab06!");
+    }
+}
